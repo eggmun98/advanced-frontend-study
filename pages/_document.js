b4690667e87da298977787161a796db91e98a75b
eggmun98 <eggmun98@gmail.com>
2023-07-26 17:29:46 +0900

    [2023.07.26] Comment 파비콘 주석 추가

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" type="image/png" href="/logo.png" />
+        {/* 위 코드는 파비콘을 적용하는 코드이다.
+        파비콘이랑 주소창 위에 나타나는 아이콘을 파비콘이라고 부른다.
+        */}
       </Head>
       <body>
         <Main />
